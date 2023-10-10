a40b5726e3b451378f04a6d473f0d0e54692d6cf
nblake707 <33844526+nblake707@users.noreply.github.com>
2023-10-09 21:30:32 -0400

    styled nav and intro section. Added favicon. gatsby-plugin-mainfest, and google fonts plugin

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link } from "gatsby";
-import { navBar, navList, linkStyle } from "./index.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+import { navBar, navList } from "./index.module.css";
 
 const Nav = () => {
   return (
-    <nav className={navBar}>
-      <ul className={navList}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/#about">About</Link>
-        </li>
-        <li>
-          <Link to="/#projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/#contact">Contacts</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav className={navBar}>
+        <StaticImage src="../../images/icon.png" alt="logo" />
+        <ul className={navList}>
+          <li>
+            <Link to="/#about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/#projects">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/#contact">
+              Contacts
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
