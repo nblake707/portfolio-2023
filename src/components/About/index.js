7c258800f980c9626c9edb640d79d1b2837d129a
nblake707 <33844526+nblake707@users.noreply.github.com>
2024-02-06 22:37:25 -0500

    Major updates to project structure and design

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Container";
+import Nav from "../Nav";
 import {
   introContainer,
   divOne,
@@ -7,74 +8,64 @@ import {
   subHeading,
   divTwo,
   textDiv,
+  imgDiv,
+  mePic,
+  toggleIcon,
 } from "./index.module.css";
-// import { StaticImage } from "gatsby-plugin-image";
-// import "./bubbles.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
 const About = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const menuToggle = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <Container id="about">
       <div className={introContainer}>
         <div className={divOne}>
           <div className={textDiv}>
-            <h1 className={mainHeading}>Nateyana Blake</h1>
+            <h1 className={mainHeading}>hello</h1>
             {/* maybe typing text here */}
             <p className={subHeading}>Web Developer and Educator</p>
           </div>
         </div>
         <div className={divTwo}>
-        {/* <StaticImage src='../../images/icon.png' alt="logo" /> */}
-          {/* <div className={`${divTwo} bubbles`}>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div> */}
+          <div 
+          // className={toggleIcon} 
+          onClick={menuToggle}>
+            {isActive ? (
+              <>
+                <StaticImage
+                  src="../../images/close-match.png"
+                  alt="Close menu icon"
+                  className={toggleIcon}
+                  width={50}
+                  height={50}
+                />
+                <Nav active={isActive}/>
+              </>
+            ) : (
+              <StaticImage
+                src="../../images/open-match.png"
+                alt="Open menu icon"
+                className={toggleIcon}
+                width={50}
+                height={50}
+              />
+            )}
+          </div>
+
+          <div className={imgDiv}>
+            <StaticImage
+              src="../../images/me-3.jpeg"
+              alt="Picture of website owner"
+              className={mePic}
+              width={450}
+              height={455}
+            />
+          </div>
         </div>
       </div>
     </Container>
