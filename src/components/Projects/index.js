106c19c032841abaa9a8a7b2dcf0cab6d708b643
nblake707 <33844526+nblake707@users.noreply.github.com>
2024-02-08 18:11:16 -0500

    Initial styling of contact section. Created 3x3 grid with photos and hover effect.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../Container";
-import { projectSection, marginDiv, projectTiles } from "./index.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+import { projectSection, marginDiv, projectTiles, projectImg } from "./index.module.css";
 
 const Projects = () => {
   return (
@@ -9,9 +10,45 @@ const Projects = () => {
       <div className={marginDiv}>
       <h2>Works</h2>
       </div>
+      {/* use a loop here to present the photos */}
       <div className={projectTiles}>
-
-</div>
+        <StaticImage 
+        src="../../images/markus-winkler-afW1hht0NSs-unsplash.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+        <StaticImage 
+        src="../../images/scott-webb-ywuH99ygfec-unsplash.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+        <StaticImage 
+        src="../../images/amy-shamblen-pJ_DCj9KswI-unsplash.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+        <StaticImage 
+        src="../../images/solen-feyissa-TaOGbz_S-Qw-unsplash.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+         <StaticImage 
+        src="../../images/red.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+         <StaticImage 
+        src="../../images/scott-webb-ywuH99ygfec-unsplash.jpg"
+        alt="Code on computer screen"
+        objectFit="cover"
+        className={projectImg}
+        />
+      </div>
       </div>
     </Container>
   );
