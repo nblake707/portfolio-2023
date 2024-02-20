912772dc7120d50ca663303c9d9bd7cbe56bb225
nblake707 <33844526+nblake707@users.noreply.github.com>
2024-02-20 16:49:03 -0500

    Updates

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,10 +5,10 @@ import { containerLayout, resumeDiv } from "./index.module.css";
 const Banner = () => {
   return (
     <section id="works" className={containerLayout}>
-    <div className={resumeDiv}>
+    {/* <div className={resumeDiv}>
         <h1>Resume</h1>
         <Button>Download</Button>
-    </div>
+    </div> */}
     </section>
   ) 
 };
