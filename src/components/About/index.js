b4604f5aaa86f7c656bfb18f15bc6149224f4f33
nblake707 <33844526+nblake707@users.noreply.github.com>
2024-02-21 17:54:36 -0500

    Made a new component for bouncing arrow. Prev setup prevented about fade in.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,22 +16,6 @@ const About = () => {
 
   return (
     <section id="about" className={containerLayout}>
-      <div className={bounce}>
-        <svg
-          className="h-10 w-10 text-teal-700"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          {" "}
-          <line x1="12" y1="5" x2="12" y2="19" />{" "}
-          <polyline points="19 12 12 19 5 12" />
-        </svg>
-      </div>
-
       <div
         ref={ref1}
         className={`${aboutSection} transition-opacity ease-in duration-1000 ${
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import Home from "../components/Home";
+import Bounce from "../components/Bounce";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Contacts from "../components/Contact";
@@ -9,6 +10,7 @@ const IndexPage = () => {
   return (
       <Layout>
         <Home />
+        <Bounce />
         <About />
         <Projects />
         <Contacts />
